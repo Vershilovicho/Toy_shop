c0898cfbb5e7553b5d4af2d1138f7b0248fce745
Vershilovicho <vernik281186@gmail.com>
2023-03-27 22:41:52 +0300

    Initial commit

---
+++ src/main/java/raffle/Main.java
@@ -0,0 +1,31 @@
+package raffle;
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+            Raffle r = new Raffle();
+            Scanner sc = new Scanner(System.in);
+            while (true) {
+                System.out.print("""
+                    Меню:
+                    1 - Пополните призовой фонд новой игрушкой
+                    2 - Измените частоту выпадения какой-нибудь игрушки
+                    3 - Проведите розыгрыш и сохраните результаты
+                    0 - Выход
+                    >\s""");
+                var choice = sc.next();
+                switch (choice) {
+                    case "1" -> r.addToy(); // лямбда-оператор, который гарантирует ,
+                    // что будет выполнен только код справа от нее. Без всякого  "проваливания".
+                    case "2" -> r.setFrequency();
+                    case "3" -> r.raffle();
+                    case "0" -> {
+                        System.out.println("Пока");
+                        System.exit(0);
+                    }
+                    default -> System.out.println("Некорректный ввод. Попробуйте снова.");
+                }
+            }
+        }
+}
+
